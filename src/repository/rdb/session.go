de38d0ecb26cfe21d316d7b47aafa6c64b8c13d2
Pandu dwi Putra <pandu@pandudpn.id>
2021-11-08 23:42:38 +0700

    create repository for redis

---
+++ src/repository/rdb/session.go
@@ -0,0 +1,40 @@
+package rdb
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/pandudpn/shopping-cart/src/domain/model"
+	"github.com/pandudpn/shopping-cart/src/utils/logger"
+	"github.com/spf13/viper"
+)
+
+func (rr *RedisRepository) SetSession(user *model.User) (string, error) {
+	expired, err := time.ParseDuration(viper.GetString("session.expired"))
+	if err != nil {
+		return "", err
+	}
+
+	key := uuid.New().String()
+
+	data := map[string]interface{}{
+		"id":    user.Id,
+		"email": user.Email,
+		"name":  user.Name,
+	}
+
+	err = rr.save(key, data, expired)
+	return key, err
+}
+
+func (rr *RedisRepository) GetSession(key string) (*model.User, error) {
+	user := model.User{}
+
+	err := rr.get(key, &user)
+	if err != nil {
+		logger.Log.Errorf("data not found in redis %v", err)
+		return nil, err
+	}
+
+	return &user, nil
+}
