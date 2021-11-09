33f3c643c6d1de2381bb42aa7b79c6bb9dc4925a
Pandu dwi Putra <pandu@pandudpn.id>
2021-11-09 12:46:03 +0700

    add to container when redis is already has

--- app/container/datastorefactory/redisFactory.go
+++ app/container/datastorefactory/redisFactory.go
@@ -3,15 +3,24 @@ package datastorefactory
 import (
 	"github.com/pandudpn/shopping-cart/app/adapter/dbc"
 	"github.com/pandudpn/shopping-cart/app/adapter/dbc/redis"
+	"github.com/pandudpn/shopping-cart/app/constant"
 	"github.com/pandudpn/shopping-cart/app/container"
+	"github.com/pandudpn/shopping-cart/src/utils/logger"
 )
 
 type redisFactory struct{}
 
 func (rf *redisFactory) Build(c container.Container, enableTx bool) (DataStoreInterface, error) {
+	if value, found := c.Get(constant.REDIS); found {
+		logger.Log.Debugf("redis found in container %v", value)
+
+		return value, nil
+	}
+
 	conn := dbc.RedisConnection()
 
 	var rdbc = &redis.RedisDb{DB: conn}
+	c.Put(constant.REDIS, rdbc)
 
 	return rdbc, nil
 }
