68ecd36d96327e0b389715752660cf88e3c2b103
Pandu dwi Putra <pandu@pandudpn.id>
2021-11-11 08:19:53 +0700

    remove unsed method

--- src/repository/user/base.go
+++ src/repository/user/base.go
@@ -11,25 +11,6 @@ type UserRepository struct {
 	DB dbc.SqlDbc
 }
 
-func retrieveUser(rows *sql.Rows) (*model.User, error) {
-	if rows.Next() {
-		return rowsToUser(rows)
-	}
-
-	return nil, nil
-}
-
-func rowsToUser(rows *sql.Rows) (*model.User, error) {
-	user := model.User{}
-
-	err := rows.Scan(&user.Id, &user.Name, &user.Email, &user.Phone, &user.Enabled, &user.EmailVerifiedAt)
-	if err != nil {
-		return nil, err
-	}
-
-	return &user, nil
-}
-
 func rowToUser(row *sql.Row) (*model.User, error) {
 	user := model.User{}
 
