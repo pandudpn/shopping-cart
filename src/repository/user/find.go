c484b6680a2a85410268d3156b45219b22b6331c
Pandu dwi Putra <pandu@pandudpn.id>
2021-11-11 08:09:51 +0700

    rename table

--- src/repository/user/find.go
+++ src/repository/user/find.go
@@ -5,7 +5,7 @@ import (
 )
 
 const (
-	QUERY_USER_SELECT   = "select id, name, email, phone, password, enabled, email_verified_at from public.user "
+	QUERY_USER_SELECT   = `select id, name, email, phone, password, enabled, email_verified_at from "user" `
 	QUERY_USER_BY_EMAIL = QUERY_USER_SELECT + "where email=$1"
 	QUERY_USER_BY_PHONE = QUERY_USER_SELECT + "where phone=$1"
 	QUERY_USER_BY_ID    = QUERY_USER_SELECT + "where id=$1"
