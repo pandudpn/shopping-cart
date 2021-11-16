4cebfc0ad0367aa695567646a5ec5c35cf33c72a
Pandu dwi Putra <pandu@pandudpn.id>
2021-11-16 23:39:14 +0700

    create cart manager for get active cart

---
+++ src/helpers/manager/base.go
@@ -0,0 +1,7 @@
+package manager
+
+import "github.com/pandudpn/shopping-cart/src/domain/model"
+
+type CartManagerInterface interface {
+	GetActiveCart(key string, userId int) (*model.Cart, error)
+}
---
+++ src/helpers/manager/cart.go
@@ -0,0 +1,57 @@
+package manager
+
+import (
+	"database/sql"
+	"errors"
+	"reflect"
+
+	"github.com/pandudpn/shopping-cart/src/domain/model"
+	"github.com/pandudpn/shopping-cart/src/repository"
+	"github.com/pandudpn/shopping-cart/src/utils/logger"
+)
+
+type cartManager struct {
+	cartRepo repository.CartRepositoryInterface
+}
+
+func NewCartManager(cart repository.CartRepositoryInterface) CartManagerInterface {
+	return &cartManager{
+		cartRepo: cart,
+	}
+}
+
+func (cm *cartManager) GetActiveCart(key string, userId int) (*model.Cart, error) {
+	activeCart := model.NewCart()
+
+	if reflect.ValueOf(key).IsZero() {
+		cart, err := cm.cartRepo.FindActiveCartByUserId(userId)
+		if err != nil {
+			if !errors.Is(err, sql.ErrNoRows) {
+				logger.Log.Errorf("error query %v", err)
+				err = errors.New("query.find.error")
+				return nil, err
+			}
+
+			return activeCart, nil
+		}
+
+		activeCart = cart
+	} else {
+		cart, err := cm.cartRepo.FindCartByKey(key)
+		if err != nil {
+			logger.Log.Errorf("error query %v", err)
+			err = errors.New("query.find.error")
+			return nil, err
+		}
+
+		if cart.UserId != userId {
+			err = errors.New("cart.not_yours")
+			return nil, err
+		}
+
+		activeCart = cart
+	}
+	// query for get all relation cart will be here
+
+	return activeCart, nil
+}
