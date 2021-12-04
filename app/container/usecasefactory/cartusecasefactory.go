cb048364b21fe9df290ed14e683eefafbd743730
Pandu dwi Putra <pandu@pandudpn.id>
2021-12-04 23:16:14 +0700

    add inject to cart usecase

--- app/container/usecasefactory/cartusecasefactory.go
+++ app/container/usecasefactory/cartusecasefactory.go
@@ -11,29 +11,69 @@ import (
 type cartUseCaseFactory struct{}
 
 func (cucf *cartUseCaseFactory) Build(c container.Container) (UseCaseFactoryInterface, error) {
+	// CART REPOSITORY
 	code := constant.CART
-
 	crif, err := repositoryfactory.GetRepositoryFbMap(code).Build(c)
 	if err != nil {
 		return nil, err
 	}
 	cri := crif.(repository.CartRepositoryInterface)
 
+	// CART PRODUCT REPOSITORY
 	code = constant.CART_PRODUCT
-
 	cprif, err := repositoryfactory.GetRepositoryFbMap(code).Build(c)
 	if err != nil {
 		return nil, err
 	}
 	cpri := cprif.(repository.CartProductRepositoryInterface)
 
+	// PRODUCT REPOSITORY
 	code = constant.PRODUCT
-
 	prif, err := repositoryfactory.GetRepositoryFbMap(code).Build(c)
 	if err != nil {
 		return nil, err
 	}
 	pri := prif.(repository.ProductRepositoryInterface)
+
+	// Product Image Repository
+	code = constant.PRODUCTIMAGE
+	pirif, err := repositoryfactory.GetRepositoryFbMap(code).Build(c)
+	if err != nil {
+		return nil, err
+	}
+	pir := pirif.(repository.ProductImageRepositoryInterface)
+
+	// User Repository
+	code = constant.USER
+	urif, err := repositoryfactory.GetRepositoryFbMap(code).Build(c)
+	if err != nil {
+		return nil, err
+	}
+	uri := urif.(repository.UserRepositoryInterface)
+
+	// User Address Repository
+	code = constant.USERADDRESS
+	uarif, err := repositoryfactory.GetRepositoryFbMap(code).Build(c)
+	if err != nil {
+		return nil, err
+	}
+	uari := uarif.(repository.UserAddressRepositoryInterface)
+
+	// Courier Repository
+	code = constant.COURIER
+	corif, err := repositoryfactory.GetRepositoryFbMap(code).Build(c)
+	if err != nil {
+		return nil, err
+	}
+	cori := corif.(repository.CourierRepositoryInterface)
+
+	// Payment Method Repository
+	code = constant.PAYMENTMETHOD
+	pmrif, err := repositoryfactory.GetRepositoryFbMap(code).Build(c)
+	if err != nil {
+		return nil, err
+	}
+	pmri := pmrif.(repository.PaymentMethodRepositoryInterface)
 
 	code = constant.TX
 
@@ -45,10 +85,15 @@ func (cucf *cartUseCaseFactory) Build(c container.Container) (UseCaseFactoryInte
 	uriTx := rufiTx.(repository.TxRepositoryInterface)
 
 	cuc := cartusecase.CartUseCase{
-		ProductRepo:     pri,
-		CartRepo:        cri,
-		CartProductRepo: cpri,
-		TxRepo:          uriTx,
+		ProductRepo:       pri,
+		CartRepo:          cri,
+		CartProductRepo:   cpri,
+		ProductImageRepo:  pir,
+		UserRepo:          uri,
+		UserAddressRepo:   uari,
+		CourierRepo:       cori,
+		PaymentMethodRepo: pmri,
+		TxRepo:            uriTx,
 	}
 
 	return &cuc, nil
